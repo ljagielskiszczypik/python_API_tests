05703304ef04daec4e12360d1de85931c31acdfe
lukas <ljagielskiszczypik@gmail.com>
2026-06-04 12:23:32 +0200

    correct report creation in conftest.py

--- conftest.py
+++ conftest.py
@@ -2,10 +2,13 @@ import pytest
 from datetime import datetime
 import os
 import json
+from pathlib import Path
 
 @pytest.hookimpl(tryfirst=True)
 def pytest_configure(config):
-    report_dir = "reports"
+    root_dir = Path(config.rootpath)
+    report_dir = root_dir / "reports"
+    report_dir.mkdir(exist_ok=True)
     time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
     config.option.htmlpath = f"{report_dir}/report_{time}.html"
 
